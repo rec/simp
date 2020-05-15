14ed296bd98a91fd83bd16a0e1bd0a670ffd6193
Tom Ritchford <tom@swirly.com>
2020-05-15 20:30:30 +0200

    New tests and fixes

--- test_simp.py
+++ test_simp.py
@@ -1,6 +1,54 @@
 from unittest import TestCase
+from pathlib import Path
+
+_SIMP = {}
+
+
+def _simp():
+    if not _SIMP:
+        text = Path('simp').read_text()
+        compiled = compile(text, 'simp', mode='exec')
+        exec(compiled, _SIMP)
+    return _SIMP
 
 
 class TestSimp(TestCase):
-    def test_simple(self):
-        pass
+    def test_lines(self):
+        actual = list(_simp()['_process_lines'](INPUT))
+        for a in actual:
+            print(a)
+        expected = OUTPUT.splitlines()
+        assert actual == expected
+
+
+INPUT = """\
+# something
+# something else
+
+from a import b
+import foo
+# comment goes ABOVE
+
+from d import f
+import foo.bar
+
+# comment goes BELOW
+
+END = 'here'
+"""
+
+OUTPUT = """\
+# something
+# something else
+
+# comment goes ABOVE
+
+from a import b
+from d import f
+import foo
+import foo.bar
+
+# comment goes BELOW
+
+END = 'here'
+"""
