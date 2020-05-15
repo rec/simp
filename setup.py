0fbed18df9bb49e372c14041a7b1d4110fc651f2
Tom Ritchford <tom@swirly.com>
2020-05-15 19:47:31 +0200

    First checkin

---
+++ setup.py
@@ -0,0 +1,36 @@
+import simp
+
+_classifiers = [
+    'Development Status :: 4 - Beta',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: MIT License',
+    'Topic :: Software Development :: Libraries',
+    'Topic :: Utilities',
+]
+
+with open('requirements.txt') as f:
+    REQUIRED = f.read().splitlines()
+
+if __name__ == '__main__':
+    from setuptools import setup
+
+    setup(
+        name='simp',
+        version=simp.__version__,
+        author='Tom Ritchford',
+        author_email='tom@swirly.com',
+        url='https://github.com/rec/simp',
+        tests_require=['pytest'],
+        py_modules=['simp'],
+        description='Sort imports simply',
+        long_description=open('README.rst').read(),
+        license='MIT',
+        classifiers=_classifiers,
+        keywords=['documentation'],
+        scripts=['simp'],
+        install_requires=REQUIRED,
+    )
---
+++ test_simp.py
@@ -0,0 +1,6 @@
+from unittest import TestCase
+
+
+class TestSimp(TestCase):
+    def test_simple(self):
+        pass
